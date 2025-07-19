8a42d0bb3ece59895d96f13375655a69d188bc6c
AsselAlan <asselalan@gmail.com>
2025-07-19 19:05:53 -0300

    Mejoras

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages';
 
-// Reemplazá 'tu-usuario' y 'tu-repo' por los tuyos
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/tu-repo/', // 👈 importante para que funcione en GitHub Pages
+  plugins: [react()],
+  base: '/chat-web-roles/', // 👈 nombre del repo con slashes
 });
